7fe3b193031d9315a37d76038ed61d8c16e88910
Indrasish Adhya <indrasishadhya770@gmail.com>
2026-05-31 17:42:48 +0530

    new

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,12 +1,10 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from .serializers import UserSerializer
 from users.models import CustomUser
-import os
 
 class SignupView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
-    permission_classes = (AllowAny,)
+    permission_classes = [AllowAny]
+    authentication_classes = []
     serializer_class = UserSerializer
