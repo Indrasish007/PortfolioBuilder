86ecf2a2bee1af3285e6b8e3c4eeeff29dfa7235
Indrasish Adhya <indrasishadhya770@gmail.com>
2026-06-03 10:48:38 +0530

    Cleanup: remove FixMigrationsView and URL mapping

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
-from .views import SignupView, EmailTokenObtainPairView, FixMigrationsView
+from .views import SignupView, EmailTokenObtainPairView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', EmailTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('fix-migrations/', FixMigrationsView.as_view(), name='fix_migrations'),
 ]
+
 
 
