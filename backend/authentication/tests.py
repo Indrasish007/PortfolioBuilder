247e75a73ef15f0d6d15b482a2304c2e33aca5e4
Indrasish Adhya <indrasishadhya770@gmail.com>
2026-06-07 15:55:51 +0530

    new deploy

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class SignupTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_signup_success(self):
+        payload = {
+            "email": "newuser@example.com",
+            "password": "strongpassword123",
+            "first_name": "New User"
+        }
+        response = self.client.post("/api/auth/signup/", payload, format="json")
+        print("SIGNUP RESPONSE STATUS:", response.status_code)
+        print("SIGNUP RESPONSE DATA:", response.data)
+        self.assertEqual(response.status_code, 201)
+
